2058f2ba0a90d535788286538565db241827232c
Kevin Xie <kevin@andui.com>
2026-01-08 19:55:28 +0800

    feat: route optimized

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.put("/user", (req, res) => {
+  res.send("Got a PUT request at /user");
+});
+
+router.post("/", (req, res) => {
+  res.send("Got a POST request");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Got a DELETE request at /user");
 });
 
 module.exports = router;
